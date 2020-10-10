acef3ddeb802b055facb6a4865faf2a70980b5ea
Fumiya Sato <fumiya.sato.rop@gmail.com>
2020-10-10 22:33:34 +0900

    mod: change to template function.

--- 001_sort/2_selection_sort/main.cpp
+++ 001_sort/2_selection_sort/main.cpp
@@ -6,13 +6,12 @@
 
 #include <iostream>
 
-#define mytype int
-
 using namespace std;
 
-void selectionSort(mytype v[], size_t size){
+template<typename T> 
+void selectionSort(T v[], size_t size){
     for (int i=0; i<size-1; i++){
-        mytype tmp = v[i];
+        T tmp = v[i];
         int    n   = i;
         for (int j=i; j<size; j++) if (v[j] < v[n]) n = j;
         v[i] = v[n];
@@ -20,7 +19,8 @@ void selectionSort(mytype v[], size_t size){
     }
 }
 
-void printArray(mytype v[], int size, string msg = ""){
+template<typename T> 
+void printArray(T v[], int size, string msg = ""){
     cout << msg;
     for(int i=0;i<size; i++) cout << v[i] << ", ";
     cout << endl;
@@ -28,7 +28,7 @@ void printArray(mytype v[], int size, string msg = ""){
 
 int main(void){
     cout << "== Selection Sort Algorithm ==" << endl;
-    mytype v[] = {10, 45, 23, 45, 65, 20, 4, 1};
+    int v[] = {10, 45, 23, 45, 65, 20, 4, 1};
     size_t sz = sizeof(v)/sizeof(*v);
     printArray(v, sz, "Input data: ");
     // Execution of selection sort
